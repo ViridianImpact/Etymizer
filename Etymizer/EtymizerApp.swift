8d24e25160eb41173133563b6233cf60eade418b
Nicholas Provencal <nicholas.provencal@gmail.com>
2026-06-25 23:47:16 -0700

    Initial Commit

---
+++ Etymizer/EtymizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtymizerApp.swift
+//  Etymizer
+//
+//  Created by Chelise Walker on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct EtymizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
